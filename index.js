cbadbd89e9eefdc888ce5155e95cc6b2fc3c5cf6
IvoSa92 <ivo_sauter@web.de>
2024-02-25 09:55:35 +0100

    (js) - added function to check the password input:    - checks  if pw is longer than 5    - checks if pw has an upper case    - checks if pw has punctuation    - checks if pw has a number
    - changed functions instead of event.target it uses now directly the input so the submit button can function properly
    
    (submit button)
    - added function to check if all validation functions returned true

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // DOM ELEMENTS:
 
 const submitButton = document.querySelector("#submit-btn");
+const form = document.querySelector("form");
 const inputs = document.querySelectorAll(".input");
 const emailInput = document.querySelector("#email-input");
 const countryInput = document.querySelector("#country-input");
@@ -8,29 +9,126 @@ const zipInput = document.querySelector("#zip-input");
 const passwordInput = document.querySelector("#password-input");
 const passwordConfInput = document.querySelector("#password-conf-input");
 
+// event listeners for the inputs with the checking functions
 emailInput.addEventListener("blur", checkMailInput);
 countryInput.addEventListener("blur", checkCountryInput);
+zipInput.addEventListener("blur", checkZipInput);
+passwordInput.addEventListener("blur", checkPassword);
+passwordConfInput.addEventListener("blur", passwordCheckingFunctions);
+form.addEventListener("submit", submitForm);
 
-function checkMailInput(e) {
-  let input = e.target;
-  if (input.value.includes("@") && input.value.length > 5) {
-    console.log("text");
-    input.classList.remove("invalid");
-    input.classList.add("valid");
+//functions for the input checks
+function checkMailInput() {
+  if (emailInput.value.includes("@") && emailInput.value.length > 5) {
+    emailInput.classList.remove("invalid");
+    emailInput.classList.add("valid");
+    return true;
   } else {
-    input.classList.remove("valid");
-    input.classList.add("invalid");
+    emailInput.classList.remove("valid");
+    emailInput.classList.add("invalid");
+    console.log("mail false");
+
+    return false;
   }
 }
 
-function checkCountryInput(e) {
-  let input = e.target;
-  if (input.value.match(/^[A-Za-z-]+$/)) {
-    console.log("valid");
-    input.classList.remove("invalid");
-    input.classList.add("valid");
+function checkCountryInput() {
+  if (countryInput.value.match(/^[A-Za-z-]+$/)) {
+    countryInput.classList.remove("invalid");
+    countryInput.classList.add("valid");
+    return true;
   } else {
-    input.classList.remove("valid");
-    input.classList.add("invalid");
+    countryInput.classList.remove("valid");
+    countryInput.classList.add("invalid");
+    console.log("country false");
+
+    return false;
+  }
+}
+
+function checkZipInput() {
+  if (zipInput.value.length > 3) {
+    zipInput.classList.remove("invalid");
+    zipInput.classList.add("valid");
+    return true;
+  } else {
+    zipInput.classList.remove("valid");
+    zipInput.classList.add("invalid");
+    console.log("zip false");
+
+    return false;
+  }
+}
+
+function checkPassword() {
+  let hasNumber = /[A-Z]/.test(passwordInput.value);
+  let hasPunctuation = /\d/.test(passwordInput.value);
+  let hasUpperCase = /[!@#$%^&*(),.?":{}|<>]/.test(passwordInput.value);
+
+  if (
+    hasNumber &&
+    hasPunctuation &&
+    hasUpperCase &&
+    passwordInput.value.length > 5
+  ) {
+    passwordInput.classList.remove("invalid");
+    passwordInput.classList.add("valid");
+    return true;
+  } else {
+    passwordInput.classList.remove("valid");
+    passwordInput.classList.add("invalid");
+    console.log("pw false");
+    return false;
+  }
+}
+
+function checkConfPassword() {
+  let hasNumber = /[A-Z]/.test(passwordConfInput.value);
+  let hasPunctuation = /\d/.test(passwordConfInput.value);
+  let hasUpperCase = /[!@#$%^&*(),.?":{}|<>]/.test(passwordConfInput.value);
+
+  if (
+    hasNumber &&
+    hasPunctuation &&
+    hasUpperCase &&
+    passwordConfInput.value.length > 5
+  ) {
+    passwordConfInput.classList.remove("invalid");
+    passwordConfInput.classList.add("valid");
+    return true;
+  } else {
+    passwordConfInput.classList.remove("valid");
+    passwordConfInput.classList.add("invalid");
+    console.log("pw conf false");
+    return false;
+  }
+}
+
+function checkPwSimilarity() {
+  if (passwordInput.value === passwordConfInput.value) {
+    console.log("passwörter sind gleich ");
+  }
+}
+
+function passwordCheckingFunctions() {
+  checkConfPassword();
+  checkPassword();
+  checkPwSimilarity();
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  const allCheckingFunctions = [
+    checkMailInput,
+    checkCountryInput,
+    checkZipInput,
+    checkPassword,
+    checkConfPassword,
+  ];
+
+  if (allCheckingFunctions.every((condition) => condition())) {
+    console.log("all true alter");
+  } else {
+    console.log("you fucked up");
   }
 }
